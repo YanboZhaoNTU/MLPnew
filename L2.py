d2789d78405f67900387be0e68c83f1d3d3f4068
YanboZhaoNTU <ZHAO0497@e.ntu.edu.sg>
2024-12-26 20:47:31 +0800

    Add files via upload
    无阈值权重

---
+++ L2.py
@@ -0,0 +1,21 @@
+import numpy as np
+# 梯度下降
+
+class WeightedStackedEnsemble:
+    def __init__(self, lr=0.01, num_iter=1000,tol=1e-5):
+        self.lr = lr
+        self.num_iter = num_iter
+        self.tol = tol
+        self.w = None
+
+    def fit(self, S, y):
+        n_samples, d = S.shape
+        self.w = np.zeros(d)
+
+        for _ in range(self.num_iter):
+            y_pred = S.dot(self.w)
+            grad = -(1.0 / n_samples) * S.T.dot(y - y_pred)
+            self.w -= self.lr * grad
+
+    def predict(self, S):
+        return S.dot(self.w)
